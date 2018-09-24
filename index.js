52c1de90b7ab237a1550fb5fb3ae1baa76ec0480
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-24 12:53:00 +0300

    Final

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ In the "onclick" event handler:
 3. Log to the console message with the name
  of the clicked menu item
 */
+const menuItems = document.querySelectorAll(".nav-link");
+const LEN = menuItems.length;
+
+for (let i = 0; i < LEN; i++) {
+  const menuItem = menuItems[i];
+
+  menuItem.onclick = function() {
+    for (let j = 0; j < LEN; j++) {
+      menuItems[j].classList.remove("active");
+    }
+
+    menuItem.classList.add("active");
+    console.log(menuItem.innerHTML);
+  };
+}
