f1ff64be2deee159d20ef75ded4e0f5b1427d54b
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-24 12:42:49 +0300

    Final

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
-// Global scope
-var a;
+// // Scope
+// let a = 10;
+// console.log(a);
 
-// Function scope
-function fn() {
-  var b;
-}
+// function fn() {
+//   let b = true;
+//   console.log(b);
+// }
 
-// Block scope
-{
-  var c;
-}
+// fn();
 
-if (true) {
-} else {
-}
+// // console.log(b);
+// {
+//   let b1 = 1;
+// }
 
-for (var i = 0; i < 5; i++) {}
+// for (let j = 0; j < 5; j++) {
+//   console.log(j);
+// }
+
+// // Reassignment
+// let c = 1;
+// c = 2;
+
+// function fn2() {
+//   c = 3;
+// }
+
+// fn2();
+
+// // Redeclaration
+// let d = false;
+// // let d = true;
+// console.log(d);
+
+// function fn3() {
+//   let d = 5;
+//   console.log(d);
+// }
+
+// fn3();
+
+// {
+//   let d = "abc";
+//   console.log(d);
+// }
+
+// console.log(d);
+
+// // Hoisting
+// let e;
+// e = 5;
+
+// function fn4() {
+//   let f;
+//   f = 10;
+//   console.log(f);
+// }
+
+// fn4();
